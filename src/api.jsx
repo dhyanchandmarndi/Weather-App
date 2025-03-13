686a28d4f302e27a6fef6a84b8c78fedd1981f9c
Dhyan Chand Marndi <141735086+dhyanchandmarndi@users.noreply.github.com>
2025-03-13 13:11:38 +0530

    Add files via upload

---
+++ src/api.jsx
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '978d4ad3acmsh4bd347acf9d53bbp13adc4jsn0483dccc8415',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "dd17db1dcc505af08d9e75631a1107ef";
